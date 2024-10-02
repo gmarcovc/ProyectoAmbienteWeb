26920b89d31127793332afa9c428da6d9b80584b
Amber Natasha Bustos Araya <103160154+AmberBustosAraya@users.noreply.github.com>
2024-10-01 21:27:10 -0600

    Cambio imagenes

--- View/home.php
+++ View/home.php
@@ -759,7 +759,7 @@
 						<div class="block2-txt flex-w flex-t p-t-14">
 							<div class="block2-txt-child1 flex-col-l ">
 								<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-									Esprit Ruffle Shirt
+									Camisa 01
 								</a>
 
 								<span class="stext-105 cl3">
@@ -909,7 +909,7 @@
 					<!-- Block2 -->
 					<div class="block2">
 						<div class="block2-pic hov-img0">
-							<img src="images/product-06.jpg" alt="IMG-PRODUCT">
+							<img src="images\product-06.jpg"alt="IMG-PRODUCT">
 
 							<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
 								Quick View
@@ -919,7 +919,7 @@
 						<div class="block2-txt flex-w flex-t p-t-14">
 							<div class="block2-txt-child1 flex-col-l ">
 								<a href="product-detail.html" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-									Vintage Inspired Classic 
+									Reloj plateado casual
 								</a>
 
 								<span class="stext-105 cl3">
