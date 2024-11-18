4f9149ef64510e4f132460205b11ac2b5dd8477a
Joel Madrigal <k.joel.mave27@gmail.com>
2024-11-18 12:44:25 -0600

    Update home.php
    -Quitar los tres cuadros que dicen "Mujer", "Hombre" y "Accesorios" (Listo)
    
    -En la sección de Ayuda, cambiar título a "Soporte" y ajustar URL de referencia (dejar igual por ahora) (Listo)

--- View/Login/home.php
+++ View/Login/home.php
@@ -320,90 +320,6 @@
 		</div>
 	</section>
 
-
-	<!-- Banner -->
-	<div class="sec-banner bg0 p-t-80 p-b-50">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<!-- Block1 -->
-					<div class="block1 wrap-pic-w">
-						<img src="../images/banner-01.jpg" alt="IMG-BANNER">
-
-						<a href="product.html.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Mujer
-								</span>
-
-								<span class="block1-info stext-102 trans-04">
-									Primavera 2025
-								</span>
-							</div>
-
-							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Compra Ahora
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-
-				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<!-- Block1 -->
-					<div class="block1 wrap-pic-w">
-						<img src="../images/banner-02.jpg" alt="IMG-BANNER">
-
-						<a href="Resenas.html.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Hombre
-								</span>
-
-								<span class="block1-info stext-102 trans-04">
-									Primavera 2025
-								</span>
-							</div>
-
-							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Compra Ahora
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-
-				<div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
-					<!-- Block1 -->
-					<div class="block1 wrap-pic-w">
-						<img src="../images/banner-03.jpg" alt="IMG-BANNER">
-
-						<a href="Resenas.html.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-							<div class="block1-txt-child1 flex-col-l">
-								<span class="block1-name ltext-102 trans-04 p-b-8">
-									Accesorios
-								</span>
-
-								<span class="block1-info stext-102 trans-04">
-									Nueva Moda 2025
-								</span>
-							</div>
-
-							<div class="block1-txt-child2 p-b-4 trans-05">
-								<div class="block1-link stext-101 cl0 trans-09">
-									Compra Ahora
-								</div>
-							</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
 	<!-- Productos y FILTER Amber -->
 	<section class="bg0 p-t-23 p-b-140">
 		<div class="container">
@@ -922,7 +838,7 @@
 			<div class="row">
 				<div class="col-sm-6 col-lg-3 p-b-50">
 					<h4 class="stext-301 cl0 p-b-30">
-						Ayuda
+						Soporte
 					</h4>
 
 					<ul>
